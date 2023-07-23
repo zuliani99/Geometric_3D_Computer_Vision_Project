c8685aa6fc3e6d468815490c8edb2e9505b726fe
Riccardo <riccardo.zuliani99@gmail.com>
2023-07-23 17:24:25 +0200

    how to find the markers position within each polygon?

--- Assignment2/marker_detector.py
+++ Assignment2/marker_detector.py
@@ -1,13 +1,41 @@
 import numpy as np
 import cv2 as cv
+import time
+import copy
+import math
 
-def print_fps():
-    pass
+objs = ['obj01.mp4']#, 'obj02.mp4', 'obj03.mp4', 'obj04.mp4']
+
+origin = (0,0)
+A = (70,0)
+angle = math.radians(-15)
+marker_reference_doords = {}
+
+def rotate(origin, point, angle):
+    """
+    Rotate a point counterclockwise by a given angle around a given origin.
+
+    The angle should be given in radians.
+    """
+    ox, oy = origin
+    px, py = point
+
+    qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
+    qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
+    return qx, qy
+
+def get_marker_reference_coords():
+    for id in range(23):
+        marker_reference_doords[id] = rotate(origin, A, id * angle)
+
 
 def find_markers():
     pass
 
-def draw_polygons(image):
+def save_stats():
+    pass
+
+def draw_red_polygons(image, actual_fps):
 	
 	imgray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 	_, thresh = cv.threshold(imgray, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
@@ -25,43 +53,81 @@ def draw_polygons(image):
 		area = cv.contourArea(cnt)
 
 		# Shortlisting the regions based on there area.
-		if area > 1730:
-			approx = cv.approxPolyDP(cnt, 0.015 * cv.arcLength(cnt, True), True)
+		if area > 1700: # 1730
+			approx = cv.approxPolyDP(cnt, 0.02 * cv.arcLength(cnt, True), True) # 0.015
 	
 			# Checking if the number of sides of the selected region is 5.
 			if(len(approx) == 5): 
-				# scaling
-				cv.drawContours(image, [approx], 0, (0, 0, 255), 3)
+    
+				cv.drawContours(image, [approx], 0, (0, 0, 255), 2)
+    
+				hull = cv.convexHull(cnt, returnPoints=False)
+
+				# Find the convexity defects in order to find the concave vertex
+				defects = cv.convexityDefects(cnt, hull)
+
+				# Check for concave corners
+				if defects is not None:
+					for i in range(defects.shape[0]):
+						_, _, f, d = defects[i, 0]
+						far = tuple(cnt[f][0])
+
+						if d > 1000: # capire perche' funziona
+							cv.line(image, (far[0], far[1] - 10), (far[0], far[1] + 10), (0,255,0), 1)
+							cv.line(image, (far[0] - 10, far[1]), (far[0] + 10, far[1]), (0,255,0), 1)
     
 	return image
 
 
 if __name__ == "__main__":
+    
+	get_marker_reference_coords()
 	
-		obj = 'obj01.mp4'
-		paused = True
-  
+	for obj in objs:
+		print(f'Marker Detector for {obj}')
 		input_video = cv.VideoCapture(f"../data/{obj}")
 
+		# Get video properties
+		frame_width = int(input_video.get(cv.CAP_PROP_FRAME_WIDTH))
+		frame_height = int(input_video.get(cv.CAP_PROP_FRAME_HEIGHT))
+		fps = input_video.get(cv.CAP_PROP_FPS)
+		tot_fps = int(input_video.get(cv.CAP_PROP_FRAME_COUNT))
+
+		actual_fps = 0
+
+		# Create output video writer
+		output_video = cv.VideoWriter(f"../output_part2/{obj.split('.')[0]}_mask.mp4", cv.VideoWriter_fourcc(*"mp4v"), fps, (frame_width, frame_height))
+  
 		while input_video.isOpened():
-      
+			start = time.time()
+			
 			# Extract a frame
 			ret, frame = input_video.read()
 
 			if not ret:	break
    
-			polygon_frame = draw_polygons(frame)
+			polygon_frame = draw_red_polygons(frame, actual_fps)
+
+			frame_with_fps = copy.deepcopy(polygon_frame)
    
-			cv.imshow('Marker Detector', polygon_frame)
+			end = time.time()
+   
+			fps = 1 / (end-start)
+			cv.putText(frame_with_fps, f"{fps:.2f} FPS", (30, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-			#if cv.waitKey(1) == ord('q'):
-				#break
+			cv.imshow(f'Marker Detector of {obj}', frame_with_fps)
+   
+			# Save frame without the FPS count in it
+			output_video.write(polygon_frame)
 
-			key = cv.waitKey(0 if paused else 10)
-			if key==32:
-				paused = not paused
+			if cv.waitKey(1) == ord('q'):
+				break
+
+			actual_fps += 1
+
 			
 		input_video.release()
+		output_video.release()
 		cv.destroyAllWindows()
 
 
