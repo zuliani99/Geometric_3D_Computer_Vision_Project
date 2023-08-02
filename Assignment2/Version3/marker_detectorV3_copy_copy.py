e4fdaf7b00382e8d2a4736088b69f24bd8dca40e
Riccardo <875532@stud.unive.it>
2023-08-02 17:09:58 +0200

    an other try

--- Assignment2/Version3/marker_detectorV3_copy_copy.py
+++ Assignment2/Version3/marker_detectorV3_copy_copy.py
@@ -1,10 +1,8 @@
-import time
-from types import NoneType
 import cv2 as cv
 import numpy as np
 
 #from utils import save_stats, set_marker_reference_coords
-from board_copy import find_interesting_points
+from board import find_interesting_points
 
 
 # List of object file name that we have to process
@@ -17,15 +15,18 @@ maxlevel = 4
 
 circle_mask_size = 11 #10
 
-def draw_mask_frame(polygons_points, frame=None, mask=None):
-	for poly in polygons_points:
-		for x, y in poly:
-			if(x != 0 and y != 0):
-				pos = (int(x), int(y))		
-				if(type(frame) != NoneType): cv.circle(frame, pos, 3, (0,0,255), -1)
-				if(type(mask) != NoneType): cv.circle(mask, pos, circle_mask_size, 255, -1)
-		if(type(frame) != NoneType): cv.drawContours(frame, np.int32([poly]), 0, (0, 0, 255), 2, cv.LINE_AA)
-	return frame, mask
+
+def order_vertices_clockwise(vertices):
+    # Calculate the centroid
+    centroid = np.mean(vertices, axis=0)
+
+    # Calculate polar angles with respect to the centroid
+    angles = np.arctan2(vertices[:, 1] - centroid[1], vertices[:, 0] - centroid[0])
+
+    # Sort the vertices based on angles (counterclockwise order)
+    sorted_indices = np.argsort(angles)
+
+    return vertices[sorted_indices[::-1]]
 
 
 def main():
@@ -41,27 +42,31 @@ def main():
 
 		actual_fps = 0
 		obj_id = obj.split('.')[0]
-  
+
 		output_video = cv.VideoWriter(f"../../output_part2/{obj_id}/{obj_id}_mask.mp4", cv.VideoWriter_fourcc(*"mp4v"), input_video.get(cv.CAP_PROP_FPS), (frame_width, frame_height))
 
-		prev_frameg = None # Previosu gray frame
-		#tracked_features = np.zeros( (0,2), dtype=np.float32 ) 
-  
+		prev_frameg = None
+		#tracked_poly_vertices = np.zeros( (0,5,2), dtype=np.float32 ) 
+
 
 		ret, frame = input_video.read()
 		frameg = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 		mask = np.zeros_like(frameg)
-		tracked_features =  find_interesting_points(frameg, mask) # (18,5,2)
-		#tracked_features = np.vstack((tracked_features, corners))
-		#print(tracked_features.shape)
+		_, tracked_poly_vertices =  find_interesting_points(frameg, mask) # -> it havbe to return 18,5,2
+		#tracked_poly_vertices = np.vstack((tracked_poly_vertices, corners))
 
-  
-		frame, mask = draw_mask_frame(tracked_features, frame, mask)
-   
+		# here I fund the initial vertex of each visible polygon using my corner detecotr
+		for poly in tracked_poly_vertices:
+			for x, y in poly:
+				pos = (int(x), int(y))		
+				cv.circle(frame, pos, 3, (0,0,255), -1) # modify the frame
+				cv.circle(mask, pos, circle_mask_size, 255, -1) # set the mask to ingnore future near detected marker
+
 		output_video.write(frame)
-   
-		prev_frameg = frameg
 
+		prev_frameg = frameg
+		
+		#print(actual_fps, tracked_poly_vertices.shape)
 
 		# Until the video is open
 		while True:
@@ -71,65 +76,76 @@ def main():
 
 			if not ret:	break
 
-   
+
 			frameg = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 			mask = np.zeros_like(frameg)
-	
-			# spread int an array of 90, 2
-			tf_OF = np.reshape(tracked_features, (tracked_features.shape[0]*tracked_features.shape[1], tracked_features.shape[2]))
-   
-			p1, st, _ = cv.calcOpticalFlowPyrLK(prev_frameg, frameg, tf_OF, None, winSize=winsize, maxLevel=maxlevel, criteria=criteria)
+
+			# here I have to reshape the traked features into a 2 dimensional array
+			tf_OF = np.reshape(tracked_poly_vertices, (tracked_poly_vertices.shape[0]*tracked_poly_vertices.shape[1], tracked_poly_vertices.shape[2]))
+
+			print(tf_OF)
+
+			p1, st, _ = cv.calcOpticalFlowPyrLK(prev_frameg, frameg, np.float32(tf_OF), None, winSize=winsize, maxLevel=maxlevel, criteria=criteria)
 			assert(p1.shape[0] == tf_OF.shape[0])
 			p0r, st0, _ = cv.calcOpticalFlowPyrLK(frameg, prev_frameg, p1, None, winSize=winsize, maxLevel=maxlevel, criteria=criteria)
 
-			fb_good = (np.fabs(p0r-tf_OF) < 0.1).all(axis=1)
+			fb_good = (np.fabs(p0r - tf_OF) < 0.1).all(axis=1)
 			fb_good = np.logical_and(np.logical_and(fb_good, st.flatten()), st0.flatten())
-			# vector of true false
-			#print(fb_good.shape)
-			#return
-   
-			#print(np.where(fb_good == False)[0]) set to zero all bad features 
-			tf_OF[np.squeeze(np.where(fb_good == False))] = np.array([0.,0.], dtype=np.float32)
-			#print(tf_OF)
-  
+
+
+			new_features_bool = np.reshape(fb_good, (int(fb_good.shape[0]/tracked_poly_vertices.shape[1]), tracked_poly_vertices.shape[1], 1))
+			lost_features_bool = np.logical_not(new_features_bool)
 			
-			# reconvert the array into a 18,5,2 array
-			tracked_features = np.reshape(tf_OF, (int(tf_OF.shape[0]/tracked_features.shape[1]), tracked_features.shape[1], tracked_features.shape[2]))
-			#print(tracked_features, tracked_features.shape)
+			actual_tracked_poly_vertices = tracked_poly_vertices * new_features_bool.astype(float)
+			lose_tracked_poly_vertices = tracked_poly_vertices * lost_features_bool.astype(float)
    
+			#print(actual_tracked_poly_vertices)
+			#print(lose_tracked_poly_vertices)
+   
+			for poly in actual_tracked_poly_vertices:
+				for x, y in poly:
+					if x!= 0. and y != 0.: cv.circle(mask, (int(x), int(y)), circle_mask_size, 255, -1)
+					# here i draw the point that LK predict 
 
 
-			_, mask = draw_mask_frame(tracked_features, mask=mask)
-		
-			tracked_features = find_interesting_points(frameg, mask, tracked_features)
-			#return
+			tracked_poly_vertices, new_polygon_coords = find_interesting_points(frameg, mask, actual_tracked_poly_vertices, lose_tracked_poly_vertices)
+			index_new_polygon = -1
    
-			print(tracked_features)
-   
-   
-			#if(corners.shape[0] > 0):
-				#frame, mask = draw_mask_frame(frame, mask, corners)
-			frame, _ = draw_mask_frame(tracked_features, frame=frame)
+			for idx, poly in enumerate(tracked_poly_vertices):
+				clock_wise_oreder = order_vertices_clockwise(poly)
+				area = cv.contourArea(np.int32(clock_wise_oreder))
+				if area < 1720:
+					tracked_poly_vertices[idx]= np.zeros((5,2), dtype=np.float32)
+					index_new_polygon = idx
+					break
+ 
+			# I found a new polygon
+			if index_new_polygon != -1: tracked_poly_vertices[index_new_polygon] = np.squeeze(new_polygon_coords[0])
+			
 
-				#tracked_features = np.vstack((tracked_features, corners)) # stack arrays in sequence vertically, for the coordinate of the cumulative features
+			for poly in tracked_poly_vertices:
+				for x, y in poly:
+					if x!= 0. and y != 0.: cv.circle(frame, (int(x), int(y)), 2, (0,0,255), -1)
 
 			prev_frameg = frameg
-   
-   
+
+
 			cv.imshow('frame',frame)
 			#cv.imshow('mask',mask)
-			
+
 			output_video.write(frame)
-   
-			if cv.waitKey(1) == ord('q'):
+
+			key = cv.waitKey(1)
+			if key == ord('p'):
+				cv.waitKey(-1) #wait until any key is pressed
+
+			if key == ord('q'):
 				break
 
 			actual_fps += 1
-   
-			time.sleep(3)
 
 		print(' DONE\n')
-  
+
 
 		# Release the input and output streams
 		input_video.release()
