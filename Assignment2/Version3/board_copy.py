fe7ceeae31239416dda6ffb1a468da72aee79e29
Riccardo <875532@stud.unive.it>
2023-08-05 15:21:01 +0200

    V3 seems ok
    TODO: to call less time the find_interesting_points

--- Assignment2/Version3/board_copy.py
+++ Assignment2/Version3/board_copy.py
@@ -1,8 +1,3 @@
-#from utils import find_distance_between_points, find_middle_point, compute_index_and_cc_coords
-from types import NoneType
-from polygon import Polygon
-
-from typing import Dict, List, Tuple, Union
 import cv2 as cv
 import numpy as np
 
@@ -13,52 +8,63 @@ zeroZone_sub = (-1, -1)
 criteria_sub = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 100, 0.001)
 
 
-def check_mask(approx_cnt, mask, lose_tracked_poly_vertices):
-	to_return = np.zeros((0,3), dtype=np.int32)
+def sort_vertices_clockwise(vertices, x_centroid=None, y_centroid=None):
+    new_centroid = np.mean(vertices, axis=0)
+    if x_centroid is None and y_centroid is None:
+        centroid = new_centroid
+    elif x_centroid is None:
+        centroid = [new_centroid[0], y_centroid]
+    elif y_centroid is None:
+        centroid = [x_centroid, new_centroid[1]]
+    else:
+        centroid = [x_centroid, y_centroid]
+        
+    angles = np.arctan2(vertices[:, 1] - centroid[1], vertices[:, 0] - centroid[0])
+    sorted_indices = np.argsort(angles)
+    return centroid, vertices[sorted_indices]
+
+
+
+def check_mask(approx_cnt, mask):
+	to_return = np.zeros((0,2), dtype=np.int32)
 	for cnt in approx_cnt:
 		x, y = cnt[0]
 		if mask[y, x] == 0: 
-			nearest_index = 0
-			if type(lose_tracked_poly_vertices) != NoneType:
-				distances = np.linalg.norm(lose_tracked_poly_vertices - np.array([x, y]), axis=-1)
-				nearest_index = np.unravel_index(np.argmin(distances), distances.shape)[0]
-			to_return = np.vstack((to_return, np.array([x, y, nearest_index]))) # x and y coordinates and index of the polygon where it must be added
+			to_return = np.vstack((to_return, np.array([x, y])))
 	return to_return
 
 
-def find_interesting_points(imgray, mask, tracked_poly_vertices=None, lose_tracked_poly_vertices=None):
+def find_interesting_points(imgray, mask):
 	_, thresh = cv.threshold(imgray, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
 	
 	# Consider only the board exluding all the object area that could be included erroneously
 	mask_thresh = np.zeros((1080, 1920), dtype=np.uint8)
-	mask_thresh[:, 1050:1600] = thresh[:, 1050:1600]
+	mask_thresh[:, 1130:1600] = thresh[:, 1130:1600]
 
 	# Finding the contourns
 	contours, _ = cv.findContours(mask_thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE) # [[[X Y]] [[X Y]] ... [[X Y]]]
 
-	new_points_of_interests = np.zeros((0,5,2), dtype=np.float32) # this array will store only the new vertices polygon found
+	points_of_interests = np.zeros( (0,2), dtype=np.float32 )
   
 	# Searching through every region selected to find the required polygon.
 	for cnt in contours:
-		
 		# Shortlisting the regions based on there area.
-		if cv.contourArea(cnt) > 1720:
+		#sorted_vertex = cnt
+		_, sorted_vertex = sort_vertices_clockwise(np.squeeze(cnt, axis=1))
+		if cv.contourArea(sorted_vertex) > 1625.0: #1685, 1650 ok 1625
+														
+			approx_cnt = cv.approxPolyDP(cnt, 0.015 * cv.arcLength(cnt, True), True) # [[[X Y]] [[X Y]] ... [[X Y]]]
 				
-			approx_cnt = cv.approxPolyDP(cnt, 0.02 * cv.arcLength(cnt, True), True) # [[[X Y]] [[X Y]] ... [[X Y]]]
-				
-			# Checking if the number of sides of the selected region is 5.
 			if (len(approx_cnt)) == 5:
-     
-				confermed_cnt = check_mask(approx_cnt, mask, lose_tracked_poly_vertices)
+				
+				if(mask.shape[0] > 0):
+					confermed_cnt = check_mask(approx_cnt, mask)
+						
+					if(confermed_cnt.shape[0] > 0):
+						ref_approx_cnt = cv.cornerSubPix(imgray, np.float32(confermed_cnt), winSize_sub, zeroZone_sub, criteria_sub)
+						points_of_interests = np.vstack((points_of_interests, np.squeeze(ref_approx_cnt)))
+				else:
+					ref_approx_cnt = cv.cornerSubPix(imgray, np.float32(approx_cnt), winSize_sub, zeroZone_sub, criteria_sub)
+					points_of_interests = np.vstack((points_of_interests, np.squeeze(ref_approx_cnt)))
 
-				if(confermed_cnt.shape[0] > 0):
-					ref_approx_cnt = cv.cornerSubPix(imgray, np.float32(confermed_cnt[:, :2]), winSize_sub, zeroZone_sub, criteria_sub)
-					if(confermed_cnt.shape[0] == 5):
-						new_points_of_interests = np.vstack((new_points_of_interests, np.expand_dims(ref_approx_cnt, axis=0)))
-					else:
-						for x, y, idx_poly in confermed_cnt:
-							first_zero_pos = np.where(tracked_poly_vertices[idx_poly] == np.zeros((1,2)[0], dtype=np.float32))[0][:-1]
-							tracked_poly_vertices[idx_poly][first_zero_pos] = np.array([x, y], dtype=np.float32)
-	return tracked_poly_vertices, new_points_of_interests
-
-
+	return points_of_interests
--- Assignment2/Version3/marker_detectorV3_copy.py
+++ Assignment2/Version3/marker_detectorV3_copy.py
@@ -1,19 +1,37 @@
 import cv2 as cv
 import numpy as np
+import random
+
 
 #from utils import save_stats, set_marker_reference_coords
-from board import find_interesting_points
+from board_copy import find_interesting_points, sort_vertices_clockwise
 
 
 # List of object file name that we have to process
-objs = ['obj01.mp4', 'obj02.mp4', 'obj03.mp4', 'obj04.mp4']
+objs = ['obj01.mp4']#, 'obj02.mp4', 'obj03.mp4', 'obj04.mp4']
 
 # se the Lucas Kanade parameters
-criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 30, 0.01)
-winsize = (5,5)
-maxlevel = 4
+criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 50, 0.01)
+winsize = (10,10)
+maxlevel = 3
 
-circle_mask_size = 11 #10
+using_laptop = False
+
+circle_mask_size = 13 # 15 quasi ok
+
+
+def random_bgr_color():
+    blue = random.randint(0, 255)
+    green = random.randint(0, 255)
+    red = random.randint(0, 255)
+    return (blue, green, red)
+
+
+def resize_for_laptop(frame, mask):
+	if using_laptop:
+		frame = cv.resize(frame, (1080, 600))
+		mask = cv.resize(mask, (1080, 600))
+	return frame, mask
 
 
 def main():
@@ -31,66 +49,167 @@ def main():
 		obj_id = obj.split('.')[0]
   
 		output_video = cv.VideoWriter(f"../../output_part2/{obj_id}/{obj_id}_mask.mp4", cv.VideoWriter_fourcc(*"mp4v"), input_video.get(cv.CAP_PROP_FPS), (frame_width, frame_height))
-
-		prev_frameg = None # Previosu gray frame
-		tracked_features = np.zeros( (0,2), dtype=np.float32 ) 
   
 
 		ret, frame = input_video.read()
 		frameg = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 		mask = np.zeros_like(frameg)
-		corners =  find_interesting_points(frameg, mask)
-		tracked_features = np.vstack((tracked_features, corners))
+		tracked_features = find_interesting_points(frameg, mask)
   
 		for x, y in tracked_features:
-			pos = (int(x), int(y))		
-			cv.circle(frame, pos, 3, (0,0,255), -1)
-			cv.circle(mask, pos, circle_mask_size, 255, -1)
+			cv.circle(frame, (int(x), int(y)), 4, (0,0,255), -1)
+			cv.circle(mask, (int(x), int(y)), circle_mask_size, 255, -1)
+   
+   
+	
+	
+		centroid, tracked_features = sort_vertices_clockwise(tracked_features, x_centroid=1300, y_centroid=550)
+		tracked_features = tracked_features[:int(tracked_features.shape[0]/5)*5,:]
+   
+   
+		cv.drawMarker(frame, np.int32(centroid), color=(255,255,255), markerType=cv.MARKER_CROSS, thickness=2)
+			
+
+
+		reshaped_cloclwise = np.reshape(tracked_features, (int(tracked_features.shape[0]/5), 5, 2))
+   
+   
+		_, last_poly_sorted = sort_vertices_clockwise(reshaped_cloclwise[-1,:,:])
+   
+		print(cv.contourArea(np.int32(last_poly_sorted)))
+  
+		if(cv.contourArea(np.int32(last_poly_sorted)) <= 1625.0):
+			to_remove = np.all(tracked_features[:, None] == reshaped_cloclwise[-1,:,:], axis=2).any(axis=1)
+			tracked_features = np.delete(tracked_features, to_remove, axis=0)
+			reshaped_cloclwise = reshaped_cloclwise[:reshaped_cloclwise.shape[0]-1, :, :]
+    
+   
+		for idx, poly in enumerate(reshaped_cloclwise):
+			# random BGR
+			color = random_bgr_color()
+			for x, y in poly:
+				cv.line(frame, (int(x), int(y)), np.int32(centroid), color, 1, cv.LINE_AA)
+			_, print_order = sort_vertices_clockwise(poly)
+			cv.drawContours(frame, np.int32([print_order]), 0, (0,0,255), 1, cv.LINE_AA)
+			cv.putText(frame, f'{str(idx)}  {cv.contourArea(np.int32(print_order))}', np.int32(print_order[0]), cv.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,0), 1, cv.LINE_AA)
+    
+   
+   
+   
+   
+		actual_fps += 1
+
+		frame_resized, mask_resized = resize_for_laptop(frame, mask)
+   
+		cv.imshow('frame', frame_resized)
+		cv.imshow('mask', mask_resized)
+   
+		print(actual_fps, 'tracked_features', tracked_features.shape[0], int(tracked_features.shape[0]/5))
+   
+		#cv.waitKey(-1)  
    
 		output_video.write(frame)
    
 		prev_frameg = frameg
 
-
-		# Until the video is open
 		while True:
 			
-			# Extract a frame
 			ret, frame = input_video.read()
 
 			if not ret:	break
 
-   
+		 
 			frameg = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 			mask = np.zeros_like(frameg)
-			
    
-			if tracked_features.shape[0] > 0: # in case I have something within my array of features to track
-       
-				p1, st, _ = cv.calcOpticalFlowPyrLK(prev_frameg, frameg, tracked_features, None, winSize=winsize, maxLevel=maxlevel, criteria=criteria)
+			
+			if(actual_fps % 15 == 0): #10 sembra abbastanza buono
+				tracked_features = find_interesting_points(frameg, mask)
+				for x, y in tracked_features: 
+					cv.circle(frame, (int(x), int(y)), 4, (0,255,255), -1)
+					cv.circle(mask, (int(x), int(y)), circle_mask_size, 255, -1)
+			else:
+		  
+				p1, st, _ = cv.calcOpticalFlowPyrLK(prev_frameg, frameg, tracked_features, None, winSize=winsize, maxLevel=maxlevel, criteria=criteria)#, flags=cv.OPTFLOW_LK_GET_MIN_EIGENVALS, minEigThreshold=0.01)
 				assert(p1.shape[0] == tracked_features.shape[0])
-				p0r, st0, _ = cv.calcOpticalFlowPyrLK(frameg, prev_frameg, p1, None, winSize=winsize, maxLevel=maxlevel, criteria=criteria)
-
+				p0r, st0, _ = cv.calcOpticalFlowPyrLK(frameg, prev_frameg, p1, None, winSize=winsize, maxLevel=maxlevel, criteria=criteria)#, flags=cv.OPTFLOW_LK_GET_MIN_EIGENVALS, minEigThreshold=0.01)
+		
 				fb_good = (np.fabs(p0r-tracked_features) < 0.1).all(axis=1)
 				fb_good = np.logical_and(np.logical_and(fb_good, st.flatten()), st0.flatten())
+					
 				tracked_features = p1[fb_good, :]
+					
+				for x, y in tracked_features: 
+					cv.circle(frame, (int(x), int(y)), 4, (0,0,255), -1)
+					cv.circle(mask, (int(x), int(y)), circle_mask_size, 255, -1)
+					
+				new_corner = find_interesting_points(frameg, mask)
+				tracked_features = np.vstack((tracked_features, new_corner)) 
 
-				for x, y in tracked_features: cv.circle(mask, (int(x), int(y)), circle_mask_size, 255, -1)
+				for x, y in new_corner: 
+					cv.circle(frame, (int(x), int(y)), 4, (0,255,255), -1)
+					cv.circle(mask, (int(x), int(y)), circle_mask_size, 255, -1)
+    
+    
+    
+																					# correct centroid
+			centroid, tracked_features = sort_vertices_clockwise(tracked_features, x_centroid=1300, y_centroid=550)
 
-			#print('tracked_features', tracked_features.shape)
+			#if(tracked_features.shape[0] < 95): tracked_features = tracked_features[:90,:]
+			#else: tracked_features = tracked_features[:95,:]
+			tracked_features = tracked_features[:int(tracked_features.shape[0]/5)*5,:]
+   
+   
+			cv.drawMarker(frame, np.int32(centroid), color=(255,255,255), markerType=cv.MARKER_CROSS, thickness=2)
 			
-			corners = find_interesting_points(frameg, mask)
+			'''if(tracked_features.shape[0] < 90):q
+				print('going to crash')
+				frame, mask = resize_for_laptop(frame, mask)
+				cv.imshow('frame',frame)
+				cv.imshow('mask',mask)
+				print('tracked_features', tracked_features.shape[0], int(tracked_features.shape[0]/5))
+				cv.waitKey(-1)'''
+
+			reshaped_cloclwise = np.reshape(tracked_features, (int(tracked_features.shape[0]/5), 5, 2))
    
-			if(corners.shape[0] > 0):
-				tracked_features = np.vstack((tracked_features, corners)) 
    
+			_, last_poly_sorted = sort_vertices_clockwise(reshaped_cloclwise[-1,:,:])
+   
+			print(cv.contourArea(np.int32(last_poly_sorted)))
+  
+			if(cv.contourArea(np.int32(last_poly_sorted)) <= 1625.0):
+				#to_remove = np.all(tracked_features[:, None] == reshaped_cloclwise[-1,:,:], axis=2).any(axis=1)
+				#tracked_features = np.delete(tracked_features, to_remove, axis=0)
+				reshaped_cloclwise = reshaped_cloclwise[:reshaped_cloclwise.shape[0]-1, :, :]
+    
+   
+			for idx, poly in enumerate(reshaped_cloclwise):
+				# random BGR
+				color = random_bgr_color()
+				for x, y in poly:
+					cv.line(frame, (int(x), int(y)), np.int32(centroid), color, 1, cv.LINE_AA)
+				_, print_order = sort_vertices_clockwise(poly)
+				cv.drawContours(frame, np.int32([print_order]), 0, (0,0,255), 1, cv.LINE_AA)
+				cv.putText(frame, f'{str(idx)}  {cv.contourArea(np.int32(print_order))}', np.int32(print_order[0]), cv.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,0), 1, cv.LINE_AA)
+    
+    
+    
+    
+    
+    
+		
 			prev_frameg = frameg
+
+			frame_resized, mask_resized = resize_for_laptop(frame, mask)
    
-			for x, y in tracked_features: cv.circle(frame, (int(x), int(y)), 3, (0,0,255), -1)
+			cv.imshow('frame', frame_resized)
+			cv.imshow('mask', mask_resized)
    
    
-			cv.imshow('frame',frame)
-			#cv.imshow('mask',mask)
+			print(actual_fps, 'tracked_features', tracked_features.shape[0], int(tracked_features.shape[0]/5))
+   
+			#cv.waitKey(-1)
+	  
 			
 			output_video.write(frame)
 
@@ -99,7 +218,7 @@ def main():
 				cv.waitKey(-1) #wait until any key is pressed
    
 			if key == ord('q'):
-				break
+				return
 
 			actual_fps += 1
 
